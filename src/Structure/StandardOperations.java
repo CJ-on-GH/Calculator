82f91397325af277a592db01cae8ee09f94cb156
Oleksii Baienko <baienko.oleksii@gmail.com>
2019-10-30 15:57:26 +0100

    add interface for standard operations

---
+++ src/Structure/StandardOperations.java
@@ -0,0 +1,22 @@
+package Structure;
+/**
+ * This interface represent the standard operations our calculator.
+ * 
+ * @author Oleksii Baienko
+ *
+ */
+public interface StandardOperations {
+	final Number memory = 0;
+	public Number add(Number a, Number b);
+	public Number substract(Number a, Number b);
+	public Number multiply(Number a, Number b);
+	public Number devide(Number a, Number b);
+	public Number invert(Number a);
+	public Number root(Number a, Number base);
+	public Number percent(Number a, Number percent);
+	public void memoryClear();
+	public Number memoryReturn();
+	public void memorySave();
+	public void memoryAdd();
+	public void memorySubstract();
+}
