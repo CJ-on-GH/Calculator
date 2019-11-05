ee5d081a9e887d65f73d133f49731ba3e5fd436a
Oleksii Baienko <oleksii.baienko@gmail.com>
2019-11-05 14:24:23 +0100

    some changes

--- src/Structure/ScientificOperations.java
+++ src/Structure/ScientificOperations.java
@@ -21,10 +21,31 @@ public interface ScientificOperations {
 	 * ================================================================
 	 */
 	
-	Double sin(Double a, boolean isInverse, char mode);
-	Double cos(Double a, boolean isInverse, char mode);
-	Double tan(Double a, boolean isInverse, char mode);
-	Double sinh(Double a, boolean isInverse, char mode);
-	Double cosh(Double a, boolean isInverse, char mode);
-	Double tanh(Double a, boolean isInverse, char mode);
+	double sin(double a, boolean isInverse, char mode);
+	double cos(double a, boolean isInverse, char mode);
+	double tan(double a, boolean isInverse, char mode);
+	double sinh(double a, boolean isInverse, char mode);
+	double cosh(double a, boolean isInverse, char mode);
+	double tanh(double a, boolean isInverse, char mode);
+	
+	/**
+	 * NOTICE:
+	 * -------
+	 * For all functions sin, cos, tan, sinh,
+	 * cosh, tanh and their inverse forms applies
+	 * ================================================================
+	 * | @param a         | A parameter of this function              |
+	 * ---------------------------------------------------------------|
+	 * | @param isInverse | True if this function is inverse          |
+	 * ---------------------------------------------------------------|
+	 * | @param mode      | A possible values of this parameter       |
+	 * |                  | are 'D'/'R'/'G' as Degrees, Radians, Grads|
+	 * ---------------------------------------------------------------|
+	 * | @return          | Result of function                        |
+	 * ================================================================
+	 */
+		
+	double log(double a, double base);
+	double lg(double a);
+	double ln(double a);
 }
--- src/Structure/StandardOperations.java
+++ src/Structure/StandardOperations.java
@@ -4,18 +4,19 @@ package Structure;
  * @author Oleksii Baienko
  */
 public interface StandardOperations {
-	Double add(Double a, Double b); // +
-	Double substract(Double a, Double b); // -
-	Double multiply(Double a, Double b); // *
-	Double devide(Double a, Double b); // /
-	Double invert(Double a); // ±
-	Double root(Double a, Double base); // √
-	Double percent(Double a, Double percent); // %
+	double add(double a, double b); // +
+	double substract(double a, double b); // -
+	double multiply(double a, double b); // *
+	double devide(double a, double b); // /
+	double invert(double a); // ±
+	double root(double a, double base); // √
+	double percent(double a, double percent); // %
 	void memoryClear(); // MC
-	Double memoryReturn(); // MR
+	double memoryReturn(); // MR
 	void memorySave(); // MS
 	void memoryAdd(); // M+
 	void memorySubstract(); // M-
 	void clear(); // C
 	void clearEntry(); // CE
+	void clearAll(); // CA
 }
